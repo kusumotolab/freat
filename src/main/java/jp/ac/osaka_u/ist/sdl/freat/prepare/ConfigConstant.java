5ec914babac9677360ed5a58e77f6cf5f6035722
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2015-06-01 23:04:19 +0900

    add an interface to have node names of the xml file

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/freat/prepare/ConfigConstant.java
@@ -0,0 +1,33 @@
+package jp.ac.osaka_u.ist.sdl.freat.prepare;
+
+public interface ConfigConstant {
+
+	public static final String NODE_NAME_ROOT = "config";
+
+	public static final String NODE_NAME_GENERAL = "general";
+
+	public static final String NODE_NAME_BATCH = "batch";
+
+	public static final String NODE_NAME_DATABASE = "database";
+
+	public static final String NODE_NAME_TARGETS = "targets";
+
+	public static final String NODE_NAME_TARGET = "target";
+
+	public static final String NODE_NAME_TARGET_NAME = "name";
+
+	public static final String NODE_NAME_REPOSITORY = "repository";
+
+	public static final String NODE_NAME_RELATIVE = "relative";
+
+	public static final String NODE_NAME_START = "start";
+
+	public static final String NODE_NAME_END = "end";
+
+	public static final String NODE_NAME_SERVLET = "servlet";
+
+	public static final String NODE_NAME_PORT = "port";
+
+	public static final String NODE_NAME_URLPATTERN = "urlpattern";
+
+}
